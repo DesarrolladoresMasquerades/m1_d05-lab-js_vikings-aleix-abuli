4b30c26887ed1ebc220cb701b26754eaa2f45c7d
Aleix Abulí <aleixabuli088@gmail.com>
2022-01-21 19:15:46 +0100

    finished

--- src/viking.js
+++ src/viking.js
@@ -63,7 +63,11 @@ class War {
     return damage;
   }
 
-  showStatus(){}
+  showStatus(){
+    if (this.vikingArmy.length === 0) return "Saxons have fought for their lives and survived another day..."
+    else if(this.saxonArmy.length === 0) return "Vikings have won the war of the century!"
+    else return "Vikings and Saxons are still in the thick of battle."
+  }
 }
 
 
